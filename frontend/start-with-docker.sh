caf043575c022c746893e3dbab23e384cc281082
Yordanos <jordan.kebede72@gmail.com>
2025-04-10 16:23:56 -0400

    chore: nuke repo – start fresh

--- frontend/start-with-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the API URL to the Docker backend
-export API_URL=http://localhost:8000
-
-# Start the Expo development server
-echo "Starting Expo with API_URL=$API_URL"
-npx expo start 
